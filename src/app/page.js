e21d1aa3771113e94d8554de4617cc6bc18b95a0
iamgaurav18 <gauravburande18@gmail.com>
2024-10-21 11:33:48 +0530

    Added keywords

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,15 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Head>
+        <title>Sexting</title>
+        <meta name="description" content="Sex chat" />
+        <meta
+          name="keywords"
+          content="Sexting, Sex chat, Adult Chat, Sexy chat"
+        />
+        <meta name="author" content="Sexting.lol" />
+      </Head>
       <main className={styles.main}>
         <ol>
           <h1>Comming Soon!</h1>
